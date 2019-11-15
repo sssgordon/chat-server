a667268856e3c88ce0d2c945b65d9eaea51dbbba
sssgordon <gso7@uwo.ca>
2019-11-15 10:26:10 +0100

    Read all messages endpoint

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ app.get("/", (req, res) => {
 
 const messages = []; // fake database
 
+app.get("/message", (req, res, next) => {
+  res.send(messages);
+});
+
 app.post("/message", (req, res, next) => {
-  const { message } = req.body; // gets rid of the body; messages=xxx <- this becomes a property of the request body
+  const { message } = req.body; // REQUIRES body-parser to use the body
+  // gets rid of the body; messages='xxx' in request <- this becomes a property of the request body
 
   messages.push(message);
 
