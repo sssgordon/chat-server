d2a5b00b3d0ffa45b9d6bf64aa6047622592341b
sssgordon <gso7@uwo.ca>
2019-11-15 10:06:20 +0100

    Create new message endpoint

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 const port = 4000;
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+const messages = []; // fake database
+
+app.post("/message", (req, res, next) => {
+  const { message } = req.body; // gets rid of the body; messages=xxx <- this becomes a property of the request body
+
+  messages.push(message);
+
+  res.send(message);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
