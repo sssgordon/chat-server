4e6bda4c0b1f05a7ddfa950706e48fe8c3c5cc7b
sssgordon <gso7@uwo.ca>
2019-11-15 10:38:28 +0100

    Stream connection endpoint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Sse = require("json-sse"); // capitalize S because server sent event is a class
 
 const app = express();
 
@@ -12,10 +13,20 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
+const stream = new Sse(); // a stream is a list of clients; send data to the stream is to send data to ALL CLIENTS
+
+app.get("/stream", (req, res, next) => {
+  // a new stream endpoint is how we get to the stream
+  const string = JSON.stringify(messages); // serialize the data is to turn the data into a series of characters (string) when we need to send data over the internet
+  stream.updateInit(string); // this updates the data when the client sees the stream; this is always placed before stream.init
+  stream.init(req, res); // a function to connect to the stream
+  // to test, run: http :4000/stream --stream
+});
+
 const messages = []; // fake database
 
 app.get("/message", (req, res, next) => {
-  res.send(messages);
+  res.send(messages); // since we don't have a database, the messages refresh everytime the server starts
 });
 
 app.post("/message", (req, res, next) => {
